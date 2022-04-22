b3e85e00fe6e790366cfe0815f20b7f25c90def9
williamvelichko <williamvelichko2003@gmail.com>
2022-04-22 15:23:36 -0700

    creating anothet table

--- migrations/20220422220808_notes.js
+++ migrations/20220422220808_notes.js
@@ -2,14 +2,17 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("notes", function (table) {
+    table.string("notes_id").unique().notNullable();
+    table.string("notes");
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("notes");
 };
--- migrations/20220422220819_cardList.js
+++ migrations/20220422220819_cardList.js
@@ -2,14 +2,17 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("cardLists", function (table) {
+    table.string("card_id").unique().notNullable();
+    table.string("task");
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("cardLists");
 };
