1b0bceaf66c5e13e645af86535ea20320954804a
williamvelichko <williamvelichko2003@gmail.com>
2022-04-22 16:37:20 -0700

    creating anothet table

--- migrations/20220422220843_taskLists.js
+++ migrations/20220422220843_taskLists.js
@@ -2,14 +2,18 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("taskLists", function (table) {
+    table.string("categorey");
+    table.string("toDo");
+    table.string("done");
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("taskLists");
 };
