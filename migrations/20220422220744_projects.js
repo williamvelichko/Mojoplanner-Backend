4045b484eeb76351c473d9d3635dc60b25f4f030
williamvelichko <williamvelichko2003@gmail.com>
2022-10-12 16:31:04 -0700

    update

--- migrations/20220422220744_projects.js
+++ migrations/20220422220744_projects.js
@@ -3,19 +3,33 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-  return knex.schema.createTable("projects", function (table) {
-    table.increments("project_id").primary();
-    table.string("project_name", 128).notNullable();
-    table.string("project_leader", 128).notNullable();
-    table
-      .integer("user_id")
-      .unsigned()
-      .notNullable()
-      .references("user_id")
-      .inTable("users")
-      .onUpdate("CASCADE")
-      .onDelete("CASCADE");
-  });
+  return knex.schema
+    .createTable("projects", function (table) {
+      table.increments("project_id");
+      table.string("project_name", 128).notNullable();
+      table.string("project_leader", 128).notNullable();
+      table
+        .integer("user_id")
+        .unsigned()
+        .notNullable()
+        .references("user_id")
+        .inTable("users")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    })
+    .createTable("tasks", function (table) {
+      table.increments("task_id");
+      table.string("task_name");
+      table.string("task_information");
+      table
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("project_id")
+        .inTable("projects")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    });
 };
 
 /**
@@ -23,5 +37,5 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-  return knex.schema.dropTableIfExists("projects");
+  return knex.schema.dropTableIfExists("tasks").dropTableIfExists("projects");
 };
